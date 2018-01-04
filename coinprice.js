36329b0c213a2ff13d21d81b8e96d024db16aa08
briko4 <31471026+briko4@users.noreply.github.com>
2018-01-05 02:19:45 +0700

    Update coinprice.js

--- coinprice.js
+++ coinprice.js
@@ -1,127 +1,43 @@
 console.log("conected");
 
-const banList = ["FLAP", "JPC", "CRB", "MC", "ERB", "EBZ", "MTLM3", "SNS", "DRS", "EMIGR", "BamitCoin", "TAT", "ACT", "OLV", "INF8", "UAEC", "SYC", "ICOO", "DTCT", "ZNA", "PZM", "ECC", "MIV", "EQB", "MOBI", "LTA", "TMT", "OPP", "ECHT", "REBL", "CRDS", "NTK", "MDL", "RBTC", "COFI", "KEX", "CVCOIN", "KCN", "NVST", "PRP", "GUESS", "CPAY", "AXT", "WSH", "MAG", "BOXY", "EARTH", "ILCT", "GAT", "GLA", "AERM", "ZEPH", "WHL", "TRIP", "SENSE", "AUT", "UTK", "MAT", "X8X", "NRO"< "FOOD", "RNDR", "LCASH", "ARBI", "NRN", "BMXT", "OROC", "BOG", "SQP", "WEB", "CJT", "MUT", "NRO", "VZT", "MSR", "XRED", "RUSTBITS", "FIL", "WAN", "DMT", "DLR", "ANTC", "BOTS", "DIM", "BULLS", "SPHTX", "SHP", "ABT", "ZAB", "C20", "MTN", "SMNX", "TTT", "XCJ", "FND", "CLOUT", "AID", "ADB", "CSTL", "XSB", "XCS", "EZM", "AMMO", "VNT", "ANCP", "SRT", "ATFS", "UTN", "STEX", "BDR", "MNZ", "MTK", "BT", "JDC", "SNK", "MASS", "BLOCKPAY", "KLK", "UP", "AUTH", "FOOD", "DUTCH", "ROCK", "TRIBE", "CABS", "SCOR", "DBR", "PYP", "EXY", "VOT", "WSC", "WYR", "EON", "CIX", "ZNT", "MNT", "AC3", "BTCL", "XUN", "xGOx", "JCR", "BITSD", "BOU", "MBT", "DRC", "RFL", "PCN", "TNB", "BNK", "JBY", "DFBT", "AMT", "3DES", "WPR", "GREA", "ICC", "JVY", "PYN", "AVA", "GEA", "ALQO", "AMIS"]
-// just a list of coins hat cause the next part - fetchData to crap out. cxorrect as of 24/12/2017
+// const banList = ["FLAP", "JPC", "CRB", "MC", "ERB", "EBZ", "MTLM3", "SNS", "DRS", "EMIGR", "BamitCoin", "TAT", "ACT", "OLV", "INF8", "UAEC", "SYC", "ICOO", "DTCT", "ZNA", "PZM", "ECC", "MIV", "EQB", "MOBI", "LTA", "TMT", "OPP", "ECHT", "REBL", "CRDS", "NTK", "MDL", "RBTC", "COFI", "KEX", "CVCOIN", "KCN", "NVST", "PRP", "GUESS", "CPAY", "AXT", "WSH", "MAG", "BOXY", "EARTH", "ILCT", "GAT", "GLA", "AERM", "ZEPH", "WHL", "TRIP", "SENSE", "AUT", "UTK", "MAT", "X8X", "NRO"< "FOOD", "RNDR", "LCASH", "ARBI", "NRN", "BMXT", "OROC", "BOG", "SQP", "WEB", "CJT", "MUT", "NRO", "VZT", "MSR", "XRED", "RUSTBITS", "FIL", "WAN", "DMT", "DLR", "ANTC", "BOTS", "DIM", "BULLS", "SPHTX", "SHP", "ABT", "ZAB", "C20", "MTN", "SMNX", "TTT", "XCJ", "FND", "CLOUT", "AID", "ADB", "CSTL", "XSB", "XCS", "EZM", "AMMO", "VNT", "ANCP", "SRT", "ATFS", "UTN", "STEX", "BDR", "MNZ", "MTK", "BT", "JDC", "SNK", "MASS", "BLOCKPAY", "KLK", "UP", "AUTH", "FOOD", "DUTCH", "ROCK", "TRIBE", "CABS", "SCOR", "DBR", "PYP", "EXY", "VOT", "WSC", "WYR", "EON", "CIX", "ZNT", "MNT", "AC3", "BTCL", "XUN", "xGOx", "JCR", "BITSD", "BOU", "MBT", "DRC", "RFL", "PCN", "TNB", "BNK", "JBY", "DFBT", "AMT", "3DES", "WPR", "GREA", "ICC", "JVY", "PYN", "AVA", "GEA", "ALQO", "AMIS"]
+// // just a list of coins hat cause the next part - fetchData to crap out. cxorrect as of 24/12/2017
 
-const currencies = []
 const data = []
-let BTCUSD = 0
-let checkList = []
 
-const compare = function (a, b) {
-  // Use toUpperCase() to ignore character casing
-  const mkcapA = parseInt(a.mkcap);
-  const mkcapB = parseInt(b.mkcap);
+const fetchData = function () {
 
-  let comparison = 0;
-  if (mkcapA > mkcapB) {
-    comparison = 1;
-  } else if (mkcapA < mkcapB) {
-    comparison = -1;
-  }
-  return comparison;
-}
+  const baseURL = "https://api.coinmarketcap.com/v1/ticker/?limit=100"
 
-
-const fetchCurrencies = function () {
-  const baseURL = "https://min-api.cryptocompare.com/data/all/coinlist";
-  const infoURL = "https://www.cryptocompare.com";
 
   $.getJSON(baseURL).done(function (info) {
-    _(info.Data).each(function (v,k) {
-      let c_obj = {
-        Symbol: v.Symbol,
-        CoinName: v.CoinName,
-        ImageUrl: infoURL + v.ImageUrl,
-        Url: infoURL + v.Url,
-        SortOrder: v.SortOrder
-      };
-      // debugger;
-
-      if (v.Symbol.indexOf("*") !== -1 ) {
-        console.log("do nothing for now with *");
-      } else if (v.Symbol.indexOf(".") !== -1 ) {
-        console.log("do nothing for now with .");
-      } else if ( banList.indexOf(v.Symbol) === -1) {
-        console.log("adding currency");
-        currencies.push(c_obj)
-      } else {
-        //
-      }
-
-
-
-    });
-    // if send too may symbols at once to get data, we get the following:
-    // "fsyms param is invalid. (fsyms length is higher than maxlength: 300)"
-    // hence we tavkle this in bites of 50.
-    $('#data').text(`loading data for ${currencies.length} currencies`)
-    let i = 0
-    let j = 49
-    // console.log("i :", i);
-    // console.log("j :", j);
-    fetchData(i,j)
-    // console.log("curr length: ",currencies.length);
-
-    while (j < currencies.length) {
-      fetchData(i,j)
-      i = i + 50
-      j = j + 50
-    }
-    // console.log("i :", i);
-    // console.log("j :", j);
-
-
-  }).fail(function () {
-    alert('Something bad happened');
-  });
-}
-
-
-
-const fetchData = function (num1, num2) {
-
-  const baseURL = "https://min-api.cryptocompare.com/data"
-  const secondPath = "/pricemultifull?fsyms=";
-  let currencySymbolList = []
-;
-
-  for (var i = (num1); i < (num2); i++) {
-    currencySymbolList.push(currencies[i].Symbol)
-  }
-
-  const compareSymbol = "&tsyms=USD"
-  $.getJSON(baseURL + secondPath + currencySymbolList.join(",") + compareSymbol).done(function (info) {
     // console.log("info is returned");
     // console.log(info);
-    _(currencySymbolList).each(function (c) {
+    _(info).each(function (c) {
       console.log(c);
+      // debugger;
       let c_obj = {
-      symbol: info.RAW[c].USD.FROMSYMBOL,
-      price: info.RAW[c].USD.PRICE,
-      change24hour: info.RAW[c].USD.CHANGEPCT24HOUR,
-      supply: info.RAW[c].USD.SUPPLY,
-      totalvolume24h: info.RAW[c].USD.TOTALVOLUME24H,
-      mkcap: info.RAW[c].USD.MKTCAP
-      // mkcap: info.RAW[c.Symbol].USD.MKTCAP
+      symbol: c.symbol,
+      price: c.price_usd,
+      change24hour: c.percent_change_24h,
+      supply: c.available_supply,
+      totalvolume24h: c['24h_volume_usd'],
+      mkcap: c.market_cap_usd
+
       }
       if (c_obj.mkcap < 1000000) {
         console.log("dont add smallest fish");
       } else if (c_obj.price < 0.1) {
         console.log("more small fish not added");
-      } else if ( checkList.indexOf(c_obj.symbol) === -1) {
-        checkList.push(c_obj.symbol)
-        data.push(c_obj)
-        data.sort(compare);
       } else {
-        // do nothing
+        data.push(c_obj)
+        // data.sort(compare);
       }
 
 
     });
-    // wait a second to get all (most) of the data to process at once...
-    setTimeout(displayData(data), 1000);
 
+    displayData(data)
   }).fail(function () {
     alert('Something bad happened');
   });
@@ -131,18 +47,19 @@ const fetchData = function (num1, num2) {
 
 const displayData = function(data) {
 
-    data = data.sort(compare).slice().reverse();
+    // data = data.sort(compare).slice().reverse();
+
 
   _(data).each(function (c_obj) {
     row = [
-	  '<tr>',
-      '<td>${c_obj.symbol}</td>',
-      '<td>${accounting.formatMoney(c_obj.price)}</td>',
-      '<td>${c_obj.change24hour.toPrecision(3)}%</td>',
-      '<td>${accounting.formatMoney(c_obj.supply)}</td>',
-      '<td>${accounting.formatMoney(c_obj.totalvolume24h)}</td>',
-      '<td>${accounting.formatMoney(c_obj.mkcap)}</td>',
-      '</tr>'
+      `<tr>`,
+      `<td>${c_obj.symbol}</td>`,
+      `<td>${accounting.formatMoney(c_obj.price)}</td>`,
+      `<td>${c_obj.change24hour}%</td>`,
+      `<td>${accounting.formatMoney(c_obj.supply)}</td>`,
+      `<td>${accounting.formatMoney(c_obj.totalvolume24h)}</td>`,
+      `<td>${accounting.formatMoney(c_obj.mkcap)}</td>`,
+      `</tr>`
     ]
     $('#data').find('tbody').append(row.join(''))
   })
@@ -154,9 +71,9 @@ const displayData = function(data) {
 $(document).ready(function() {
   console.log("document ready");
 
-  $('#data').text('loading data...')
+  $('#data').text(`loading data...`)
 
-  fetchCurrencies()
+  fetchData()
 
 
 })
